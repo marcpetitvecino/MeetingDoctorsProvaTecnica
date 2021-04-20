b5322578bf1f69158648d2f7e7e064aec1259d2e
marcpetitvecino <marc.petit@mataro.epiaedu.cat>
2021-04-21 01:29:28 +0200

    Desisted on using coroutines, using basic approach, text is being loaded correctly

--- app/src/main/java/com/example/meetingdoctorsprovatecnica/MainActivity.kt
+++ app/src/main/java/com/example/meetingdoctorsprovatecnica/MainActivity.kt
@@ -2,23 +2,24 @@ package com.example.meetingdoctorsprovatecnica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStream
+import java.io.InputStreamReader
 import java.util.*
 
-private const val FIRST_FILE_NAME = "com\\example\\meetingdoctorsprovatecnica\\files\\alice29.txt"
-private const val SECOND_FILE_NAME = "com\\example\\meetingdoctorsprovatecnica\\files\\Nombres.txt"
-private const val THIRD_FILE_NAME = "com\\example\\meetingdoctorsprovatecnica\\files\\plrabn12.txt"
+private const val FIRST_FILE_NAME = "alice29.txt"
+private const val SECOND_FILE_NAME = "Nombres.txt"
+private const val THIRD_FILE_NAME = "plrabn12.txt"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var loadButton: AppCompatButton
-    private lateinit var list: RecyclerView
+    private lateinit var wordList: RecyclerView
     private lateinit var adapter: Adapter
     private var running = false
 
@@ -36,7 +37,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
         loadButton = findViewById(R.id.loadBtn)
-        list = findViewById(R.id.list)
+        wordList = findViewById(R.id.list)
     }
 
 
@@ -46,45 +47,33 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadList() {
+    private  fun loadList() {
         running = true
-        GlobalScope.launch {
-            adapter = Adapter(getFilesContent().toList())
-            list.adapter = adapter
-        }
         adapter = Adapter(listOf("Loading..."))
-        list.adapter = adapter
-        running = false
+        wordList.adapter = adapter
+
+        adapter = Adapter(getFilesContent())
+        wordList.adapter = adapter
     }
 
-
     private fun getFilesContent(): LinkedList<String?> {
-        var bufferedReader: BufferedReader = File(FIRST_FILE_NAME).bufferedReader()
         val list = LinkedList<String?>()
 
-        GlobalScope.launch {
-            bufferedReader.useLines {
-                firstFileContent = it.toString()
-                list.add(firstFileContent)
-                }
-            }
-
-        GlobalScope.launch {
-            bufferedReader = File(SECOND_FILE_NAME).bufferedReader()
-            bufferedReader.useLines {
-                    secondFileContent = it.toString()
-                    list.add(secondFileContent)
-            }
+        application.assets.open(FIRST_FILE_NAME).bufferedReader().use {
+            firstFileContent = it.readText()
         }
 
-        GlobalScope.launch {
-            bufferedReader = File(THIRD_FILE_NAME).bufferedReader()
-            bufferedReader.useLines {
-                thirdFileContent = it.toString()
-                list.add(thirdFileContent)
-            }
+        application.assets.open(SECOND_FILE_NAME).bufferedReader().use {
+            secondFileContent = it.readText()
         }
 
+        application.assets.open(THIRD_FILE_NAME).bufferedReader().use {
+            thirdFileContent = it.readText()
+        }
+
+        list.add(firstFileContent)
+        list.add(secondFileContent)
+        list.add(thirdFileContent)
         return list
     }
 
